1d3844bb25369e31d6952e11cc75dbce06deeffc
Bo Vyper <150873969+donaldneusplukker@users.noreply.github.com>
2023-11-17 08:03:56 -0600

    updation

---
+++ gamesScript.js
@@ -0,0 +1,6 @@
+var games = [
+	{ name: 'snake', description: '', img: './games/snake/snake.png', active: false },
+	{ name: '', description: '', img: './games/snake/snake.png', active: false },
+	{ name: '', description: '', img: './games/snake/snake.png', active:false },
+]
+
